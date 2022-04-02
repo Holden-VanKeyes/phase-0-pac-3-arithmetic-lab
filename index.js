49bbc6373c79a02d309e45d45a80a7556a9ca9d9
abramshook <ajshook77@gmail.com>
2022-04-02 12:58:38 -0500

    commit tree 17a3ee91f2e6d679fe8397358f28bff53def5dcd

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(x , y)
+{return x + y};
+function subtract(a, b)
+{return a - b};
+function multiply(a, b) 
+{return a * b};
+function divide(a, b)
+{return a / b};
+function increment(number)
+{number === 10;
+    return number += 1};
+    function decrement(number)
+{number === 10;
+    return number -= 1};
+    function makeInt(string) 
+    {
+        return parseInt(string , 10);
+    };
+    function preserveDecimal(string)
+    {string === 2.222
+    return parseFloat(string)}
